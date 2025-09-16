073bfe85ccec7e0a839dde325cc58136c03307d3
Elle Simonds <ellesimonds@gmail.com>
2025-09-16 11:43:52 -0500

    Added ListView widget.

--- app/src/main/java/model/Pet.java
+++ app/src/main/java/model/Pet.java
@@ -1,4 +1,39 @@
 package model;
 
 public class Pet {
+    String name;
+    String breed;
+    int age;
+
+//Constructor
+    public Pet(String name, String breed, int age) {
+        this.name = name;
+        this.breed = breed;
+        this.age = age;
+    }
+
+// Getters and Setters
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBreed() {
+        return breed;
+    }
+
+    public void setBreed(String breed) {
+        this.breed = breed;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
 }
